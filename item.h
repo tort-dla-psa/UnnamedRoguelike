1bf50d3fd4dcdf737301fa93c569427959250042
Alexeev Eugene <essirandluna@gmail.com>
2018-02-17 00:41:06 +0300

    short to ushort, gameobject inheritance

--- item.h
+++ item.h
@@ -1,43 +1,20 @@
 #ifndef item_h
 #define item_h
 #include<string>
-class tile;
-class creature;
-class material;
-class item{
-	short price,volume,rarity;
-	short sharpness;
-	char img;
-	creature* owner;
-	creature* holder;
-	tile* place;
-	material* materia;
-	std::string quality;
-	std::string name;
+#include"gameobject.h"
+
+class item:public gameobjectmovable{
+        ushort price,rarity;
+        std::string quality;
 public:
-	item(std::string name, char img, short volume, short sharpness, material* materia);
-	~item();
-	void SetQuality(std::string quality);
-	void SetRarity(short rarity);
-	void SetPrice(short price);
-	void Transfer(creature* hewholder);
-	void Gift(creature* newowner);
-	void Move(tile* newplace);
-	void Move(creature* newholder);
-	std::string GetQuality();
-	std::string GetName();
-	char GetImg();
-	short GetX();
-	short GetY();
-	short GetZ();
-	short GetPrice();
-	short GetWeight();
-	short GetVolume();
-	short GetRarity();
-	short GetSharpness();
-	creature* GetOwner();
-	creature* GetHolder();
-	tile* GetPlace();
-	material* GetMaterial();
+        item(std::string name, char img, material* materia);
+        ~item();
+        void SetQuality(std::string quality);
+        void SetRarity(short rarity);
+        void SetPrice(short price);
+        std::string GetQuality();
+        ushort GetPrice();
+        ushort GetRarity();
 };
+
 #endif
