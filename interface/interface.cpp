4a2200a9fefc4a70b4f2e10862fd5de921683f52
Alexeev Eugene <essirandluna@gmail.com>
2018-02-27 20:57:53 +0300

    deleted lists class, added keys and keycodes for key handling, cam is part of the interface, added attack dialog

--- interface/interface.cpp
+++ interface/interface.cpp
@@ -1,22 +1,29 @@
 #include<ncurses.h>
 #include<string>
+#include<algorithm>
 #include"interface.h"
 #include"MyColor.h"
 #include"MyPair.h"
-#include"lists.h"
 #include"windows.h"
+#include"KeyTie.h"
+#include"camera.h"
+#include"../gameobject.h"
+#include"../creature.h"
 
 //test
 interface::~interface(){
 	delete playerstat,
 	       chat,
 	       mainscr;
+	wins.clear();
 	for(auto c:colors){
 		delete c;
 	}
+	colors.clear();
 	for(auto p:pairs){
 		delete p;
 	}
+	pairs.clear();
 	clear();
 	endwin();
 }
@@ -58,10 +65,113 @@ interface::interface(){
 	mainscr = new worldwindow(width,height+1);
 	playerstat = new window_playerstats(width/6-1, height-10, width/6, 10);
 	chat = new window_chat(0,height-10,width/6,10,50);
+	attackdialog = nullptr;
+	wins.push_back(mainscr);
+	wins.push_back(playerstat);
+	wins.push_back(chat);
+	SetFocus(mainscr);
 }
 
+void interface::AddKey(char key, int keycode){
+	keys.push_back(new keytie(key,keycode));
+}
+int interface::HandleKey(char key){
+	for(auto k:keys){
+		if(k->GetChar()==key){
+			const int code = k->GetCode();
+			if(mainscr->IsFocused()){
+				if(!cam->Flying()){
+					if(code==iKEY_UP){
+						return iKEYCODE_PLAYER_MOVE_UP;
+					}else if(code==iKEY_DOWN){
+						return iKEYCODE_PLAYER_MOVE_DOWN;
+					}else if(code==iKEY_LEFT){
+						return iKEYCODE_PLAYER_MOVE_LEFT;
+					}else if(code==iKEY_RIGHT){
+						return iKEYCODE_PLAYER_MOVE_RIGHT;
+					}else if(code==iKEY_ATTACK_UP){
+						return iKEYCODE_PLAYER_ATTACK_UP;
+					}else if(code==iKEY_ATTACK_DOWN){
+						return iKEYCODE_PLAYER_ATTACK_DOWN;
+					}else if(code==iKEY_ATTACK_LEFT){
+						return iKEYCODE_PLAYER_ATTACK_LEFT;
+					}else if(code==iKEY_ATTACK_RIGHT){
+						return iKEYCODE_PLAYER_ATTACK_RIGHT;
+					}else if(code==iKEY_PICK_UP){
+						return iKEYCODE_PLAYER_PICK_UP;
+					}else if(code==iKEY_OPEN_INVENTORY){
+						SetFocus(playerstat);
+					}else if(code==iKEY_OPEN_CHAT){
+						SetFocus(chat);
+					}else if(code==iKEY_CAMERA_FLY){
+						cam->Follow((gameobjectstatic*)player->GetPlace());
+					}
+				}else{
+					if(code==iKEY_UP){
+						return iKEYCODE_CAM_MOVE_UP;
+					}else if(code==iKEY_DOWN){
+						return iKEYCODE_CAM_MOVE_DOWN;
+					}else if(code==iKEY_LEFT){
+						return iKEYCODE_CAM_MOVE_LEFT;
+					}else if(code==iKEY_RIGHT){
+						return iKEYCODE_CAM_MOVE_RIGHT;
+					}else if(code==iKEY_DOWNZ){
+						return iKEYCODE_CAM_MOVE_DOWNZ;
+					}else if(code==iKEY_UPZ){
+						return iKEYCODE_CAM_MOVE_UPZ;
+					}else if(code==iKEY_ESC){
+						cam->Follow(player);
+					}else if(code==iKEY_OPEN_INVENTORY){
+						SetFocus(playerstat);
+					}else if(code==iKEY_OPEN_CHAT){
+						SetFocus(chat);
+					}
+				}
+			}else if(attackdialog){
+				if(code==iKEY_UP){
+					AttackDialogUp();
+				}else if(code==iKEY_DOWN){
+					AttackDialogDown();
+				}else if(code==iKEY_ENTER){
+					player->Attack(GetAttackDialogTarget());
+				}else if(code==iKEY_ESC){
+					AttackDialogClose();
+				}
+			}else if(playerstat->IsFocused()){
+				if(code==iKEY_ESC){
+					SetFocus(mainscr);
+				}else if(code==iKEY_DROP){
+					player->Drop(GetHighlight());
+					short oldhlght = GetHighlight();
+					InventoryFocusRight();
+					if(oldhlght==GetHighlight())
+						InventoryFocusLeft();
+				}else if(code==iKEY_LEFT){
+					InventoryFocusLeft();
+				}else if(code==iKEY_RIGHT){
+					InventoryFocusRight();
+				}else if(code==iKEY_PICK_UP){
+					player->Take(GetHighlight());
+				}
+			}else if(chat->IsFocused()){
+				if(code==iKEY_PGUP){
+					ChatScrollUp();
+				}else if(code==iKEY_PGDOWN){
+					ChatScrollDown();
+				}else if(code==iKEY_ESC){
+					SetFocus(mainscr);
+				}
+			}
+		}
+	}
+}
 void interface::AssignPlayer(creature* player){
 	playerstat->AssignPlayer(player);
+	if(cam){
+		delete cam;
+	}
+	cam = new camera(player);
+	this->player = player;
 }
 
 void interface::CheckResize(){
@@ -76,6 +186,8 @@ void interface::Draw(){
 	chat->Draw();
 	playerstat->Draw();
 	playerstat->PrintStats();
+	if(attackdialog)
+		attackdialog->Draw();
 	doupdate();
 	move(GetHeight()-1,GetWidth()-1);
 }
@@ -92,8 +204,34 @@ void interface::DrawOnMap(short x, short y,char ch, short color){
 		mainscr->Draw(x,y,ch, 8+color);
 	}
 }
-void interface::ShowDialog(std::string mes, std::vector<std::string> variants){
-	
+void interface::ShowAttackDialog(std::vector<gameobjectmovable*> targets){
+	short width,height;
+	getmaxyx(stdscr,height,width);
+	ushort winx = width/6*2-1;
+	ushort winy = height-10;
+	attackdialog = new attack_dialog(winx,winy,width/6,10,targets);
+	wins.push_back(attackdialog);
+	SetFocus(attackdialog);
+}
+void interface::AttackDialogUp(){
+	attackdialog->FocusUp();
+}
+void interface::AttackDialogDown(){
+	attackdialog->FocusDown();
+}
+gameobjectmovable* interface::GetAttackDialogTarget(){
+	gameobjectmovable* target = attackdialog->GetFocused();
+	AttackDialogClose();
+	return target;
+}
+bool interface::GetAttackDialogFocus(){
+	return (attackdialog);
+}
+void interface::AttackDialogClose(){
+	wins.erase(std::find(wins.begin(),wins.end(),attackdialog),wins.end());
+	delete attackdialog;
+	attackdialog = nullptr;
+	SetFocus(mainscr);
 }
 void interface::WriteToChat(std::string text){
 	chat->AddMessage(text);
@@ -107,12 +245,17 @@ void interface::ChatScrollUp(){
 void interface::ChatScrollDown(){
 	chat->ScrollDown();
 }
-void interface::SetChatFocus(bool key){	chat->SetFocused(key);}
+void interface::SetFocus(window* win){
+	for(auto w:wins){
+		if(w==win){
+			w->SetFocused(true);
+		}else{
+			w->SetFocused(false);
+		}
+	}
+}
 bool interface::GetChatFocus(){		chat->IsFocused();}
 
-void interface::SetInventoryFocus(bool key){	
-	playerstat->SetFocused(key);
-}
 void interface::InventoryFocusLeft(){
 	playerstat->FocusLeft();
 }
@@ -136,8 +279,17 @@ void interface::ResizeInterface(short newx, short newy){
 }
 void interface::ProcessWindowOnResize(window_bordered* win, short deltax, short deltay){
 	if(win!=NULL){
-			win->MoveAt(0,deltay);
-			win->MoveAt(deltax,0);
+		win->MoveAt(0,deltay);
+		win->MoveAt(deltax,0);
 	}
 }
 
+short interface::GetCamX(){	return cam->GetX();}
+short interface::GetCamY(){	return cam->GetY();}
+short interface::GetCamZ(){	return cam->GetZ();}
+short interface::GetCamOffsetX(){	return cam->GetOffsetX();}
+short interface::GetCamOffsetY(){	return cam->GetOffsetY();}
+void interface::CamFollow(gameobjectmovable* target){	cam->Follow(target);}
+void interface::CamFollow(gameobjectstatic* target){	cam->Follow(target);}
+bool interface::CamFlying(){	return cam->Flying();}
+void interface::SetCamParameters(short width, short height){	cam->SetParams(width,height);}
