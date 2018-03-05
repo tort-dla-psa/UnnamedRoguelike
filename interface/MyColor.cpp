d7b50de55d15c8ca381ce1f48ea97404aa6bb357
Alexeev Eugene <essirandluna@gmail.com>
2018-03-05 18:48:15 +0300

    removed color names

--- interface/MyColor.cpp
+++ interface/MyColor.cpp
@@ -1,8 +1,8 @@
 #include<ncurses.h>
 #include"MyColor.h"
 
-myColor::myColor(short r, short g, short b, std::string name, short num):
-        r(r),g(g),b(b),name(name),num(num)
+myColor::myColor(short r, short g, short b, short num):
+        r(r),g(g),b(b),num(num)
 {
         init_color(num,r,g,b);
 }
@@ -13,5 +13,4 @@ short myColor::GetR(){          return r;}
 short myColor::GetG(){          return g;}
 short myColor::GetB(){          return b;}
 short myColor::GetNum(){        return num;}
-std::string myColor::GetName(){ return name;}
 
--- interface/MyColor.h
+++ interface/MyColor.h
@@ -1,15 +1,12 @@
 #ifndef color_h
 #define color_h
-#include<string>
 
 class myColor{
         short r,g,b,num;
-        std::string name;
 public:
-        myColor(short r, short g, short b, std::string name, short num);
+        myColor(short r, short g, short b, short num);
         myColor();
         ~myColor();
-        std::string GetName();
         short GetNum();
         short GetR();
         short GetG();
